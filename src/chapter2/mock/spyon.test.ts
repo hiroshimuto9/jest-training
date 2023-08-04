2434d9fa50bbcb9dfc9ef0800c9160c7cabac2d7
Hiroshi Muto <mhi6o461o@gmail.com>
2023-08-04 15:42:23 +0900

    learn about jest.spyOn and reset mock

---
+++ src/chapter2/mock/spyon.test.ts
@@ -0,0 +1,16 @@
+describe('Math.random with spyOn', () => {
+  let spy
+  afterEach(() => {
+    spy.mockRestore() // モック関数を元の関数へ戻す
+    // jest.restoreAllMocks() // すべてのモック化した関数をオリジナルの関数へ戻す
+  })
+
+  it('Math.random return 1', () => {
+    spy = jest.spyOn(Math, 'random').mockImplementation(() => 1)
+    expect(Math.random()).toBe(1);
+  })
+
+  it('Math.random return under 1', () => {
+    expect(Math.random()).toBeLessThan(1);
+  })
+})
